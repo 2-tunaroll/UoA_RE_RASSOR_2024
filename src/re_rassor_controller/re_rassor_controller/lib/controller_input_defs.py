81433d663343978147a41f77c6561aa0e71bf342
teresavkelly <teresa.kelly.02@icloud.com>
2024-08-22 09:33:18 +0930

    update controller input defs

--- src/re_rassor_controller/re_rassor_controller/lib/controller_input_defs.py
+++ src/re_rassor_controller/re_rassor_controller/lib/controller_input_defs.py
@@ -1,27 +1,27 @@
 # PS4 controller input definitions
 
 # Axes
-LEFT_JOY_HORIZONTAL = 0
-LEFT_JOY_VERTICAL = 1
-RIGHT_JOY_HORIZONTAL = 2
-RIGHT_JOY_VERTICAL = 3
-LEFT_TRIGGER = 4
-RIGHT_TRIGGER = 5
+LEFT_JOY_HORIZONTAL:int= 0
+LEFT_JOY_VERTICAL:int= 1
+RIGHT_JOY_HORIZONTAL:int= 2
+RIGHT_JOY_VERTICAL:int= 3
+LEFT_TRIGGER:int = 4
+RIGHT_TRIGGER:int= 5
 
 # Buttons
-CROSS = 0
-CIRCLE = 1
-SQUARE = 2
-TRIANGE = 3
-SHARE = 4
-PS = 5
-OPTIONS = 6
-L_JOY_IN = 7
-R_JOY_IN = 8
-L1 = 9
-R1 = 10
-UP = 11
-DOWN = 12
-LEFT = 13
-RIGHT = 14
-TOUCH_PAD = 15
+CROSS:int = 0
+CIRCLE:int = 1
+SQUARE:int = 2
+TRIANGE:int = 3
+SHARE:int = 4
+PS:int = 5
+OPTIONS:int = 6
+L_JOY_IN:int = 7
+R_JOY_IN:int = 8
+L1:int = 9
+R1:int = 10
+UP:int = 11
+DOWN:int = 12
+LEFT:int = 13
+RIGHT:int = 14
+TOUCH_PAD:int = 15
