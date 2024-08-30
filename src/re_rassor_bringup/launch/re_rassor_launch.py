63fa05c9a780bff88ec6873a613d7c4feb80e58e
teresavkelly <teresa.kelly.02@icloud.com>
2024-08-30 11:34:49 +0930

    update launch pkg

--- src/re_rassor_bringup/launch/re_rassor_launch.py
+++ src/re_rassor_bringup/launch/re_rassor_launch.py
@@ -5,20 +5,19 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import launch
-import launch_ros.actions
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
+    return LaunchDescription([
+        Node(
             package='re_rassor_sensors',
             executable='accelerometer',
             name='accelerometer'),
-        launch_ros.actions.Node(
+        Node(
             package='re_rassor_sensors',
             executable='internal_temperature',
             name='internal_temperature'),
-        launch_ros.actions.Node(
+        Node(
             package='re_rassor_sensors',
             executable='external_temperature',
             name='external_temperature'),
