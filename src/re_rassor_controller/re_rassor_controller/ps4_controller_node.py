157184e1dc04baace4ca0e4bb3f11fb05490eb74
teresavkelly <teresa.kelly.02@icloud.com>
2024-09-02 12:11:39 +0930

    update controller data processing:

--- src/re_rassor_controller/re_rassor_controller/ps4_controller_node.py
+++ src/re_rassor_controller/re_rassor_controller/ps4_controller_node.py
@@ -1,38 +1,33 @@
 import socket
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Int32MultiArray, Int16
-
+from std_msgs.msg import String, Int16
+import time
 from geometry_msgs.msg import Twist
 import json
 import re_rassor_controller.lib.controller_input_defs as inputs
-import time
+
+from custom_msgs.msg import TJoint, BucketDrum
 
 class ControllerCommandPublisher(Node):
     def __init__(self):
 
-        super().__init__('json_publisher')
+        super().__init__('controller_command_publisher')
 
         self.controller_state_publisher_ = self.create_publisher(String, 'controller_state', 100)
 
         self.velocity_publisher_ = self.create_publisher(Twist, 'cmd_vel', 100)
-        self.t_joint_publisher_ = self.create_publisher(Int32MultiArray, 't_joint_cmd', 100)
-        self.bucket_drum_publisher_ = self.create_publisher(Int32MultiArray, 'bucket_drum_cmd', 100)
-        self.tool_interchange_publisher_ = self.create_publisher(Int16, 'tool_interchange_cmd', 100)
+        self.t_joint_publisher_ = self.create_publisher(TJoint, 't_joint_cmd', 100)
+        self.bucket_drum_publisher_ = self.create_publisher(BucketDrum, 'bucket_drum_cmd', 100)
+        self.tool_interchange_publisher_ = self.create_publisher(Int16, 'tool_interchange_cmd', 10)
         self.vibrating_motor_publisher_ = self.create_publisher(Int16, 'vibrating_motor_cmd', 100)
         # self.speed_mode_publisher = self.create_publisher(Float32, 'speed_mode')
 
         self.debounce_time = 0.5 #seconds
         self.circle_last_pressed_time = 0 
-        self.t_joint_selection = 'FRONT'
-
-
-        self.t_joint_msg = Int32MultiArray()
-        self.t_joint_msg.data = [0, 0, 0]
-
-
-        self.bucket_drum_msg = Int32MultiArray()
-        self.t_joint_msg.data = [0, 0]
+        self.cross_last_pressed_time = 0
+        self.t_joint_msg = TJoint()
+        self.t_joint_msg.t_joint.data = 'FRONT'
 
         self.receive_data()
 
@@ -110,51 +105,73 @@ class ControllerCommandPublisher(Node):
     def get_t_joint_commands(self, data):
 
         current_time = time.time()
-        debounce_time = 0.2 # seconds
+        debounce_time = 0.5 # seconds
 
-        if (data['buttons'][inputs.CIRCLE] == 1) and (current_time - self.circle_last_pressed_time > debounce_time):
+        # toggle between t-joints
+        if data['buttons'][inputs.CIRCLE] == 1:
 
-            self.circle_last_pressed_time = current_time
+            # Only toggle if the button wasn't previously pressed
+            if not self.circle_button_pressed and (current_time - self.circle_last_pressed_time > debounce_time):
+                self.circle_last_pressed_time = current_time
 
-            if self.t_joint_msg.data[0] == 0:
-                self.t_joint_msg.data[0] = 1 # front t-joint
+                # Toggle the state
+                if self.t_joint_msg.t_joint.data == 'FRONT':
+                    self.t_joint_msg.t_joint.data = 'BACK'
+                elif self.t_joint_msg.t_joint.data == 'BACK':
+                    self.t_joint_msg.t_joint.data = 'FRONT'
 
-            else:
-                self.t_joint_msg.data[0] = 0 # back t-joint
+                # Set the flag to indicate the button is now pressed
+                self.circle_button_pressed = True
+
+        # Check if the button is released
+        elif data['buttons'][inputs.CIRCLE] == 0:
+            # Reset the flag when the button is released
+            self.circle_button_pressed = False
 
         # must be pressing L2 and R2 to deliver power
         if data['axes'][inputs.RIGHT_TRIGGER] > -0.95 and data['axes'][inputs.LEFT_TRIGGER] > -0.95:
             
-            self.t_joint_msg.data[1] = data['buttons'][inputs.L1] # up
-            self.t_joint_msg.data[2] = data['buttons'][inputs.R1] # down
+            # only publish up or down at one time
+            if self.t_joint_msg.down != 1:
+                self.t_joint_msg.up = data['buttons'][inputs.L1] # up
+
+            if self.t_joint_msg.up != 1:
+                self.t_joint_msg.down = data['buttons'][inputs.R1] # down
         
         self.t_joint_publisher_.publish(self.t_joint_msg)
         
     def get_tool_commands(self, data):
         
         current_time = time.time()
-        debounce_time = 0.2 # seconds\
+        debounce_time = 0.2 # seconds
 
         tool_interchange_msg = Int16()
         vibrating_motor_msg = Int16()
+        bucket_drum_msg = BucketDrum()
 
         # tool interchange
-        if (data['buttons'][inputs.CROSS] == 1) and (current_time - self.circle_last_pressed_time > debounce_time):
-            tool_interchange_msg = data['buttons'][inputs.CROSS]
+        if (data['buttons'][inputs.CROSS] == 1) and (current_time - self.cross_last_pressed_time > debounce_time):
+            tool_interchange_msg.data = data['buttons'][inputs.CROSS]
+            self.cross_last_pressed_time = current_time
 
         # must be pressing L2 and R2 to deliver power
         if data['axes'][inputs.RIGHT_TRIGGER] > -0.95 and data['axes'][inputs.LEFT_TRIGGER] > -0.95:
             
             # bucket drum
-            self.bucket_drum_msg.data[0] = data['buttons'][inputs.UP] # up
-            self.bucket_drum_msg.data[1] = data['buttons'][inputs.DOWN] # down
+            # only publish forward or back at one time
+            if bucket_drum_msg.backward != 1:
+                # print(Int16(data['buttons'][inputs.UP]))
+                bucket_drum_msg.forward = data['buttons'][inputs.UP] # forward
+                # print(bucket_drum_msg.forward)
 
+            if bucket_drum_msg.forward != 1:
+                bucket_drum_msg.backward = data['buttons'][inputs.DOWN] # backward
+            
             # vibrating motor
             vibrating_motor_msg.data = data['buttons'][inputs.TRIANGLE]
 
-        
         # publish commands
-        self.bucket_drum_publisher_.publish(self.bucket_drum_msg)
+        self.bucket_drum_publisher_.publish(bucket_drum_msg)
         self.tool_interchange_publisher_.publish(tool_interchange_msg)
         self.vibrating_motor_publisher_.publish(vibrating_motor_msg)
            
