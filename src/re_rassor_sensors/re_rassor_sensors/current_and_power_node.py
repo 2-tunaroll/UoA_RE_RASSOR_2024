7f437cd59d6d57cea55dd473ca21e0309e8db1e3
teresavkelly <teresa.kelly.02@icloud.com>
2024-09-10 22:30:15 +0930

    add basic current and power sensing

--- src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
+++ src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
@@ -13,13 +13,25 @@ class CurrentandPowerNode(Node):
         i2c = board.I2C()  # uses board.SCL and board.SDA
         self.ina260 = INA260(i2c) # default address 0x40
 
-        self.shutdown_publisher = self.create_publisher(Bool, 'shutdown_cmd', 10)
-        self.voltage_publisher = self.create_publisher(Int16, 'battery_voltage', 10)
+        self.shutdown_publisher_ = self.create_publisher(Bool, 'shutdown_cmd', 10)
+        self.voltage_publisher_ = self.create_publisher(Int16, 'battery_voltage', 10)
 
         self.get_current_and_voltage()
 
     def get_current_and_voltage(self):
-        pass
+
+        # current
+        current_msg = Bool()
+        if self.ina260.current > 8000:
+            current_msg.data = True
+        else:
+            current_msg.data = False
+        self.shutdown_publisher_.publish(current_msg)
+        
+        # voltage
+        voltage_msg = Int16()
+        voltage_msg.data = self.ina260.voltage
+        self.voltage_publisher_.publish(voltage_msg)
 
 def main(args=None):
 
