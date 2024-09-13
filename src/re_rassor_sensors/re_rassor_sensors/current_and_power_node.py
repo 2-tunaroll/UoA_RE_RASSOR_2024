5212d012b0c013ed457a8e6107a8c8905eaebece
teresavkelly <teresa.kelly.02@icloud.com>
2024-09-13 14:08:57 +0930

    Update current and power sensing

--- src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
+++ src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
@@ -1,9 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from re_rassor_sensors.lib.adafruit_ina260 import INA260
-from std_msgs.msg import Bool, Int16
+from re_rassor_sensors.lib.adafruit_ina260 import INA260, AveragingCount
+from std_msgs.msg import Bool, Int16, Float32
 import board
 import os
+from statistics import mean
 
 class CurrentandPowerNode(Node):
     def __init__(self):
@@ -14,31 +15,67 @@ class CurrentandPowerNode(Node):
         i2c = board.I2C()  # uses board.SCL and board.SDA
         self.ina260 = INA260(i2c) # default address 0x40
 
-        self.shutdown_publisher_ = self.create_publisher(Bool, 'shutdown_cmd', 10)
-        self.voltage_publisher_ = self.create_publisher(Int16, 'battery_voltage', 10)
+        # 0x1770 x 1.25 mA = 7500 mA as alert limit
+        self.ina260.alert_limit = 0x1770
 
-        self.get_current_and_voltage()
+        # assert alert pin
+        self.ina260.overcurrent_limit = True
 
-    def get_current_and_voltage(self):
+        # keep the flag high until MASK_ENABLE register will be read
+        self.ina260.alert_latch_enable = True        
 
-        # current
-        current_msg = Bool()
-        if self.ina260.current > 7500:
-            current_msg.data = True
+        # list to hold 4x voltage values
+        self.voltage_array = []
+
+        # self.pi_shutdown_publisher_ = self.create_publisher(Bool, 'pi_shutdown_cmd', 10)
+        self.motor_shutdown_publisher_ = self.create_publisher(Bool, 'motor_shutdown_cmd', 10)
+
+        self.voltage_publisher_ = self.create_publisher(Float32, 'battery_voltage', 10)
+        self.current_publisher_ = self.create_publisher(Float32, 'system_current', 10)
+
+        self.timer_1 = self.create_timer(0.05, self.get_current_alert) # run every 0.05 s
+        self.timer_2 = self.create_timer(1, self.get_voltage_and_current) # run every 1s
+
+    def get_voltage_and_current(self):
+
+        # create battery voltage msg
+        battery_voltage_msg = Float32()
+
+        # get the current voltage
+        current_battery_voltage = self.ina260.voltage
+
+        # add voltages to the array to check for low battery
+        if len(self.voltage_array) < 4:
+            self.voltage_array.append(current_battery_voltage)
+        if len(self.voltage_array) == 4:
+            # average and check voltage for low battery
+            average_voltage = mean(self.voltage_array)
+            self.voltage_array.clear()
+
+            # shutdown the Raspberry Pi if the voltage over the given time period drops below 14.8V
+            if average_voltage <= 10: # change to what this needs to be
+                os.system("sudo shutdown -h now")
+                return
+
+        # publish the battery voltage
+        battery_voltage_msg.data = current_battery_voltage
+        self.voltage_publisher_.publish(battery_voltage_msg)
+
+        # system current
+        system_current = Float32()
+        system_current.data = self.ina260.current
+        self.current_publisher_.publish(system_current)
+
+    def get_current_alert(self):
+
+        shutdown_flag = Bool()
+
+        if self.ina260.alert_function_flag:
+            shutdown_flag = True
         else:
-            current_msg.data = False
-        self.shutdown_publisher_.publish(current_msg)
-        
-        # voltage
-        voltage_msg = Int16()
-        voltage_msg.data = self.ina260.voltage
+            shutdown_flag = False
 
-        # shutdown the Raspberry Pi if the voltage drops below 14.8V
-        if voltage_msg.data <= 14.8:
-            os.system("sudo shutdown -h now")
-            return
-
-        self.voltage_publisher_.publish(voltage_msg)
+        self.motor_shutdown_publisher_.publish(shutdown_flag)
 
 def main(args=None):
 
