cc1fd85e61d8591a52a22f772e26e06a72300c0a
teresavkelly <teresa.kelly.02@icloud.com>
2024-09-16 17:56:35 +0930

    fix shutdown_cmd topic name

--- src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
+++ src/re_rassor_sensors/re_rassor_sensors/current_and_power_node.py
@@ -1,9 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from re_rassor_sensors.lib.adafruit_ina260 import INA260, AveragingCount
-from std_msgs.msg import Bool, Int16, Float32
+from re_rassor_sensors.lib.adafruit_ina260 import INA260
+from std_msgs.msg import Bool, Float32
 import board
-import os
 from statistics import mean
 
 class CurrentandPowerNode(Node):
@@ -27,7 +26,7 @@ class CurrentandPowerNode(Node):
         # list to hold 4x voltage values
         self.voltage_array = []
 
-        self.motor_shutdown_publisher_ = self.create_publisher(Bool, 'motor_shutdown_cmd', 10)
+        self.motor_shutdown_publisher_ = self.create_publisher(Bool, 'shutdown_cmd', 10)
 
         self.voltage_publisher_ = self.create_publisher(Float32, 'battery_voltage', 10)
         self.current_publisher_ = self.create_publisher(Float32, 'system_current', 10)
@@ -51,8 +50,8 @@ class CurrentandPowerNode(Node):
             average_voltage = mean(self.voltage_array)
             self.voltage_array.clear()
 
-            # shutdown the Raspberry Pi if the voltage over the given time period drops below 12V
-            if average_voltage <= 12: # change to what this needs to be
+            # shutdown the Raspberry Pi if the voltage over the given time period drops below 12.8V
+            if average_voltage <= 12.8: # change to what this needs to be
                 print("low battery: shutting down!!")
                 # os.system("sudo shutdown -h now")
                 return
