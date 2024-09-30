c0c74804535f0052f02446366e02442e3fcc895d
teresavkelly <teresa.kelly.02@icloud.com>
2024-09-30 14:27:41 +0930

    add try-except to temperature probe

--- src/re_rassor_sensors/re_rassor_sensors/external_temperature_node.py
+++ src/re_rassor_sensors/re_rassor_sensors/external_temperature_node.py
@@ -3,45 +3,49 @@ import glob
 import time
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64 
+from std_msgs.msg import Float64
 
 class ExternalTemperatureNode(Node):
     def __init__(self):
          
         super().__init__('temperature_node')
-        self.publisher_c = self.create_publisher(Float64, 'external_temperature_c', 10)
-        # self.publisher_f = self.create_publisher(Float64, 'external_temperature_f', 10)
-        self.timer = self.create_timer(1.0, self.publish_temperature)
-            
-        
-    def read_temp_raw(self):
 
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
         
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
-        device_file = device_folder + '/w1_slave'
+        self.device_file = device_folder + '/w1_slave'
 
-        f = open(device_file, 'r')
+        self.publisher_c = self.create_publisher(Float64, 'external_temperature_c', 10)
+        # self.publisher_f = self.create_publisher(Float64, 'external_temperature_f', 10)
+        self.timer = self.create_timer(1.0, self.publish_temperature)
+        
+    def read_temp_raw(self):
+
+        f = open(self.device_file, 'r')
         lines = f.readlines()
         f.close()
         return lines
 
     def publish_temperature(self):
-
-        lines = self.read_temp_raw()
-        while (lines[0].strip())[-3:] != 'YES':
-            time.sleep(0.2)
+        try:
             lines = self.read_temp_raw()
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            while lines[0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                lines = self.read_temp_raw()
+            equals_pos = lines[1].find('t=')
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos+2:]
+                temp_c = float(temp_string) / 1000.0
+                temp_f = temp_c * 9.0 / 5.0 + 32.0
 
-            self.publisher_c.publish(Float64(data=temp_c))
-            # self.publisher_f.publish(Float64(data=temp_f))
+                self.publisher_c.publish(Float64(data=temp_c))
+                # self.publisher_f.publish(Float64(data=temp_f))
+            else:
+                    self.get_logger().warn("Temperature reading not found in sensor data. Skipping.")
+        except IndexError as e:
+            self.get_logger().error(f"Index error occurred: {e}. Skipping this iteration.")
 
 def main(args=None):
     rclpy.init(args=args)
